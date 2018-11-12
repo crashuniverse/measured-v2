ff04f3deb34ba21b5614693e27ff6a4bdbc6a7b8
Priya Ranjan Singh <mail@priyaranjan.net>
2018-11-12 16:34:09 +0100

    feat: Modify controller to transform weights data

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,19 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.scss']
 })
+
 export class ViewComponent implements OnInit {
 
-  weights: AngularFireList<any[]>;
+  weightsRef: AngularFireList<any[]>;
+  weights: Observable<any[]>;
+  recentWeights: Observable<RecentWeightItem[]>;
 
-  constructor(private af: AngularFireDatabase) {
-    this.weights = af.list('/').valueChanges();
+  constructor(private af: AngularFireDatabase) { }
+
+  ngOnInit() {
+    this.weightsRef = this.af.list('weights');
+    this.weights = this.weightsRef.valueChanges();
+    this.recentWeights = this.weights.pipe(
+      map(item => {
+        return item.map((s) => {
+          return new RecentWeightItem(s._created_at && s._created_at.date, s.text);
+        });
+      })
+    );
   }
+}
 
-  ngOnInit() {}
+class RecentWeightItem {
+  createdAt: Date;
+  text: string;
 
+  constructor(createdAt: string, text: string) {
+    this.createdAt = new Date(createdAt);
+    this.text = text;
+  }
 }
